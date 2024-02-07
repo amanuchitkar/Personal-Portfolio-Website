1685ace810be2218d2c03f31ebcffcceeb100e41
aman uchitkar <amanuchitkar@gmail.com>
2024-02-07 18:12:15 +0530

    update

--- Portfolio/settings.py
+++ Portfolio/settings.py
@@ -116,7 +116,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
+
 STATIC_URL = 'static/'
+STATIC_ROOT = 'staticfiles'
 STATICFILES_DIRS = [
     BASE_DIR / "static"
     ]
